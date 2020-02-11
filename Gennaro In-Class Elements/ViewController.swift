ece22a5e78b2d1aae5e1c78e39959cc30687310b
Olivia Gennaro <o.gennaro@setonhill.edu>
2020-02-11 11:22:51 -0500

    Initial Commit

---
+++ Gennaro In-Class Elements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gennaro In-Class Elements
+//
+//  Created by Olivia Gennaro on 2/11/20.
+//  Copyright © 2020 Olivia Gennaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
